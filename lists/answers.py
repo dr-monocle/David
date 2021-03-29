32f9e36c46494dd21d81958e2235bf47632f9838
Francisco Oliveira <74855391+franciscomgoliveira@users.noreply.github.com>
2021-03-29 14:20:07 +0100

    Uploaded logotype and lists

---
+++ lists/answers.py
@@ -0,0 +1,4 @@
+timeSL = ['Right now it is ', 'It is ', 'Now, it is ']
+timePL = ['Right now it\'s ', 'It\'s ', 'Now, it\'s ']
+
+errorL = ['Please repeat', 'Can you repeat, please?', 'Sorry, I didn\'t understand', 'Sorry, can you repeat, please?']
---
+++ lists/questions.py
@@ -0,0 +1,3 @@
+timeL = ['what time is it', 'what time is', 'what is the time', 'what is the time now', 'tell me the time', 'tell me what time is it', 'tell me what time is', 'tell me what is the time', 'tell me what is the time now', 'please what time is it', 'please tell me the time']
+
+exitL = ['shutdown', 'close', 'stop']
