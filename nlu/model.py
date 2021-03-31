aff27d1e7c2deac3ba217771d3839e4b29ffa7a3
Francisco Oliveira <74855391+franciscomgoliveira@users.noreply.github.com>
2021-03-31 12:05:37 +0100

    Adding data to model.py

--- nlu/model.py
+++ nlu/model.py
@@ -1,6 +1,12 @@
 import yaml
 import json
 import numpy as np
+import os
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.utils import to_categorical
 
 data = yaml.safe_load(open('nlu/train.yml', 'r').read())
 
@@ -9,20 +15,42 @@ data = yaml.safe_load(open('nlu/train.yml', 'r').read())
 inputs, outputs = [], []
 
 for command in data['commands']:
-    inputs.append(command['input'])
+    inputs.append(command['input'].lower())
     outputs.append('{}\{}'.format(command['entity'], command['action']))
 
 # Creating a dataset
 
     # Choosing a level of tokenization: words, chars, BPEs
 chars = set()
-    # Read all chair in the dataset
+    # Read all chars in the dataset
 for i in inputs + outputs:
     for ch in i:
         if ch not in chars:
             chars.add(ch)
+    
+    # Map each char to an index
+char2idx = {}
+idx2char = {}
 
-    # Create an input data
+for k, ch in enumerate(chars):
+    char2idx[ch]
+    idx2char[k]
 
+    # Create input data
+max_sent = max([len(x) for x in inputs])
+    
+    # Create arrays
+input_data = np.zeros((len(inputs), max_sent, len(chars)), dtype='int32')
 
-print(len(chars))
+for i, input in enumerate(inputs):
+    for k, ch in enumerate(input):
+        input_data[i, k, char2idx[ch]]
+
+output_data = np.eye()
+# output_data = to_categorical(output_data, len(output_data))
+
+print(output_data[0])
+
+#print(input_data.shape)
+# print(len(chars))
+# print('Max input seq: ', max_sent)
